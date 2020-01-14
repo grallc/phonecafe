9fc5dc4dc45675dc62d5d93da0b2883f914298ab
Corentin Grall <corentin.grall@gmail.com>
2020-01-14 10:59:44 +0100

    Handling products delection

--- public/store/js/main.js
+++ public/store/js/main.js
@@ -12,14 +12,25 @@ $(document).ready(function () {
         $('<td>').text(item.os),
         $('<td>').html(`${item.screensize}" (<abbr>inches</abbr>)`),
         $('<td>').html(productImage),
-        $('<td>').html('<button class="action"><i class="icon solid fa-edit" /></button><button class="action"><i class="icon solid fa-trash-alt" /></button>')
+        $('<td>').html('<button type="button" class="action"><i class="icon solid fa-edit" /></button><button class="action delete-action" type="button"><i class="icon solid fa-trash-alt" /></button>')
       ).prependTo('#products-table')
     })
   }, 'json')
 
   // Add the label for range slider.
-  $('#screensize-range').on('input', function () {
+  $(document).on('input', '#screensize-range', function () {
     $('#screensize-range-value').html($(this).val() + '"')
+  })
+
+  $(document).on('click', '.delete-action', function () {
+    const parent = $(this).parent().parent()
+    $.ajax({ url: `/products/${parent.attr('product-id')}`, method: 'DELETE' })
+      .then(function (data) {
+        parent.remove()
+      })
+      .catch(function (err) {
+        console.log(err)
+      })
   })
 
   $('.sorting-item')
